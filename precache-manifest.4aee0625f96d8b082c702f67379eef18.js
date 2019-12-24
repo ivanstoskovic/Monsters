1c99963e5e75c4d5caa47dae3c42946b0d5ad155
Ivan Stoskovic <ivst0406@enjoying.rs>
2019-12-24 11:30:20 +0100

    Updates

---
+++ precache-manifest.4aee0625f96d8b082c702f67379eef18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318869706f71591cccc0a2ba715bbcab",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "0735f5ba51aabdd91a0a",
+    "url": "/Monsters/static/css/main.3e8f0315.chunk.css"
+  },
+  {
+    "revision": "1b43d2eaa999d2a50f77",
+    "url": "/Monsters/static/js/2.c698f0ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters/static/js/2.c698f0ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0735f5ba51aabdd91a0a",
+    "url": "/Monsters/static/js/main.41aff42e.chunk.js"
+  },
+  {
+    "revision": "f9e5ef9c249f8b27b6ea",
+    "url": "/Monsters/static/js/runtime-main.47d1c7e9.js"
+  }
+]);
